53690efb6f046df30e01b84350db17dad563ad15
Carlosgt2307 <carlosagonzalestorres@gmail.com>
2024-12-13 19:20:07 -0500

    Fix: Register xml

--- app/src/main/java/com/example/eduread/ui/view/RegisterActivity.kt
+++ app/src/main/java/com/example/eduread/ui/view/RegisterActivity.kt
@@ -1,6 +1,11 @@
 package com.example.eduread.ui.view
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -12,10 +17,22 @@ class RegisterActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_register)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        // Referencias a los campos
+       val usernameEditText = findViewById<EditText>(R.id.nameEditText)
+        val ageEditText = findViewById<EditText>(R.id.ageEditText)
+        val passwordEditText = findViewById<EditText>(R.id.passwordEditText)
+        val registerButton = findViewById<Button>(R.id.registerButton)
+
+        // Listener del botón (sin lógica por ahora)
+        registerButton.setOnClickListener {
+            // Aquí iría la lógica de registro con la API en el futuro.
         }
     }
 }
