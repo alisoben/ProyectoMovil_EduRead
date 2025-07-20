5ca2dd11efa32d10c141bf1d282c8bb17275907f
alisoben <alder.galvez@unmsm.edu.pe>
2025-07-19 19:07:43 -0500

    Guardando cambios antes de actualizar main

--- app/src/main/java/com/example/eduread/network/RetrofitClient.kt
+++ app/src/main/java/com/example/eduread/network/RetrofitClient.kt
@@ -6,7 +6,7 @@ import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 
 object RetrofitClient {
-    private const val BASE_URL = "https://edu-read.onrender.com/"
+    private const val BASE_URL = "http://10.0.2.2:8000/"
 
     private val loggingInterceptor = HttpLoggingInterceptor().apply {
         level = HttpLoggingInterceptor.Level.BODY
